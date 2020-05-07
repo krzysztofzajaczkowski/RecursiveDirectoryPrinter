6ef2423c57d7251c5899d57feac87afe61945f6c
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2020-05-08 00:42:57 +0200

    RD-5: Add serialization to/deserialization from file passed as second argument

--- RecursiveDirectoryPrinter/RecursiveDirectoryPrinter/Program.cs
+++ RecursiveDirectoryPrinter/RecursiveDirectoryPrinter/Program.cs
@@ -10,6 +10,13 @@ namespace RecursiveDirectoryPrinter
             processor.RecursivePrint(args[0], 0);
             Console.WriteLine($"Oldest file: {processor.GetOldestFileInDirectory(args[0])}");
             var sortedDict = processor.GetSortedDirectChildren(args[0]);
+            var serializer = new Serializer();
+            serializer.SerializeToFile(args[1], sortedDict);
+            var deserializedDict = serializer.DeserializeFromFile(args[1]);
+            foreach (var keyValuePair in deserializedDict)
+            {
+                Console.WriteLine($"{keyValuePair.Key} -> {keyValuePair.Value}");
+            }
         }
     }
 }
