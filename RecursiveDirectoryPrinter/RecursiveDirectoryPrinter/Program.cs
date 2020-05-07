3606d9724a8e291164f1bd2fdb0c29908bbf315d
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2020-05-07 20:14:16 +0200

    Upload initial project

---
+++ RecursiveDirectoryPrinter/RecursiveDirectoryPrinter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RecursiveDirectoryPrinter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
