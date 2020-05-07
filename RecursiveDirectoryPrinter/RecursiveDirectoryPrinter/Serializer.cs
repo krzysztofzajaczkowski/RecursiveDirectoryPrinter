081c96b518f30440e66ee1e227bab2936e0ead0e
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2020-05-08 00:35:27 +0200

    RD-5: Add Serializer class with SerializeToFile method

---
+++ RecursiveDirectoryPrinter/RecursiveDirectoryPrinter/Serializer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace RecursiveDirectoryPrinter
+{
+    public class Serializer
+    {
+        public void SerializeToFile(string filePath, IDictionary<string, int> dictionary)
+        {
+            FileStream fileStream = new FileStream(filePath, FileMode.Create);
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            try
+            {
+                binaryFormatter.Serialize(fileStream, dictionary);
+            }
+            catch (SerializationException e)
+            {
+                Console.WriteLine($"Serialization error: {e}");
+            }
+            fileStream.Close();
+        }
+    }
+}
