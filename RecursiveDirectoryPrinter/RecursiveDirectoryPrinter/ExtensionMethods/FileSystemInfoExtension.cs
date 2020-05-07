a5ba89396c0d3e33717d8d493488ce8fff6195aa
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2020-05-08 00:04:06 +0200

    RD-3: Add FileSystemInfoExtension with GetRahs method

---
+++ RecursiveDirectoryPrinter/RecursiveDirectoryPrinter/ExtensionMethods/FileSystemInfoExtension.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace RecursiveDirectoryPrinter.ExtensionMethods
+{
+    public static class FileSystemInfoExtension
+    {
+        public static string GetRahs(this FileSystemInfo fileSystemInfo)
+        {
+            char[] rahs = {'-', '-', '-', '-'};
+            FileAttributes fileAttributes = fileSystemInfo.Attributes;
+            if ((fileAttributes & FileAttributes.ReadOnly) != 0)
+            {
+                rahs[0] = 'r';
+            }
+
+            if ((fileAttributes & FileAttributes.Archive) != 0)
+            {
+                rahs[1] = 'a';
+            }
+
+            if ((fileAttributes & FileAttributes.Hidden) != 0)
+            {
+                rahs[2] = 'h';
+            }
+
+            if ((fileAttributes & FileAttributes.System) != 0)
+            {
+                rahs[3] = 's';
+            }
+
+            return string.Concat(rahs);
+        }
+    }
+}
