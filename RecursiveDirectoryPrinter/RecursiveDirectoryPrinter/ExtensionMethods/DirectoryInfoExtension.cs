fd87476b16306da815b107ff1b596f7ff45c46a0
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2020-05-07 22:00:17 +0200

    RD-2: Add DirectoryInfoExtension with GetOldestChildrenDate

---
+++ RecursiveDirectoryPrinter/RecursiveDirectoryPrinter/ExtensionMethods/DirectoryInfoExtension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace RecursiveDirectoryPrinter.ExtensionMethods
+{
+    public static class DirectoryInfoExtension
+    {
+        public static DateTime GetOldestChildrenDate(this DirectoryInfo directory)
+        {
+            var oldestElementDate = DateTime.Now;
+
+            foreach (var directoryInfo in directory.GetDirectories())
+            {
+                var directoryOldestElementDate = directoryInfo.GetOldestChildrenDate();
+                if (directoryOldestElementDate < oldestElementDate)
+                {
+                    oldestElementDate = directoryOldestElementDate;
+                }
+            }
+
+            foreach (var fileInfo in directory.GetFiles())
+            {
+                var fileCreationDate = fileInfo.CreationTime;
+                if (fileCreationDate < oldestElementDate)
+                {
+                    oldestElementDate = fileCreationDate;
+                }
+            }
+
+            return oldestElementDate;
+        }
+    }
+}
